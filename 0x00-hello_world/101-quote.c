3b9bf8bdc2ef7468e433b69abacc29f1dad3d6ff
Alfred <alfredoseibossman99@gmail.com>
2023-03-16 20:56:34 +0000

    updated 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * Return: Always 1 (Success)
- * This program prints a string of text to stderr
+ * This program prints som tests to stderr
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" -Dora Korpar, 2015-10-19");
 	return (1);
 }
