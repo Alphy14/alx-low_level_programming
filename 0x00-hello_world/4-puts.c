71b6f7629e181dca9c38c9b929468747eb31e0ad
Alfred <alfredoseibossman99@gmail.com>
2023-03-16 19:03:18 +0000

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * This program makes use of the puts () function
+ * it prints out the content of the variable Mywork
+ */
+int main(void)
 {
-	char Mywork="Programming is like building a multilingual puzzle";
+	char Mywork[] = "Programming is like building a multilingual puzzle";
+
 	puts(Mywork);
-	return 0;
+	return (0);
 }
