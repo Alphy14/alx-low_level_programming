dc6390d66ee4ac4800af7a2336e3def6c8155eca
Alfred <alfredoseibossman99@gmail.com>
2023-03-17 19:29:31 +0000

    created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
