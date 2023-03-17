3b221fb5643b3245f44175f3706e3089fcdf783c
Alfred <alfredoseibossman99@gmail.com>
2023-03-17 21:49:03 +0000

    updated 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,15 @@ int main(void)
 {
 	int num;
 
-	num = 0;
-	while (num <= 9)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
-		num = num + 1;
-		putchar(',');
-		putchar(' ');
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
