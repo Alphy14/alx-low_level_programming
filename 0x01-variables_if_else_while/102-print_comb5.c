b3c0f92bf50900855b75679b0e4aedcd07e533ed
Alfred <alfredoseibossman99@gmail.com>
2023-03-20 10:21:09 +0000

    created 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 9; num2++)
+		{
+			for (num3 = 0; num3 <= 9; num3++)
+			{
+				for (num4 = num3 + 1; num4 <= 9; num4++)
+				{
+					putchar('0' + num1);
+					putchar('0' + num2);
+					putchar(' ');
+					putchar('0' + num3);
+					putchar('0' + num4);
+					if ((num1 != 9) || (num2 != 8) || (num3 != 9) || (num4 != 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
