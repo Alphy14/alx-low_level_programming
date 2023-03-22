663001fded0269ceab9b330faac5a89e33729c72
Alfred <alfredoseibossman99@gmail.com>
2023-03-22 10:15:18 +0000

    created 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: No return value
+ */
+void jack_bauer(void)
+{
+	int num1, num2;
+
+	num1 = 0;
+	while (num1 < 24)
+	{
+		num2 = 0;
+		while (num2 < 60)
+		{
+			_putchar((num1 / 10) + '0');
+			_putchar((num1 % 10) + '0');
+			_putchar(':');
+			_putchar((num2 / 10) + '0');
+			_putchar((num2 % 10) + '0');
+			_putchar('\n');
+			num2++;
+		}
+		num1++;
+	}
+}
