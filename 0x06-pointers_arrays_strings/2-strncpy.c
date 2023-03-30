3931938d30a954d32d58bfea966dbc17e46f7cd1
Alfred <alfredoseibossman99@gmail.com>
2023-03-30 15:13:33 +0000

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Specified number of strings to be copied
+ * Return: Copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	int srclen = 0;
+
+	while (src[index++])
+		srclen++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = srclen; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
