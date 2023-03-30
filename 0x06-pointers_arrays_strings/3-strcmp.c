11b930248b1d79e7b76e7ed82e12127d82a6523d
Alfred <alfredoseibossman99@gmail.com>
2023-03-30 15:43:41 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int index;
 
-	for (index = 0; s1[index] != '\0' || s2[index] != '\0'; index++)
+	index = 0;
+	while (s1[index] != '\0' && s2[index] != '\0')
 	{
 		if (s1[index] != s2[index])
-		{
-			if (s1[index] < s2[index])
-				return (s1[index] - s2[index]);
-			else if (s1[index] > s2[index])
-				return (s1[index] - s2[index]);
-		}
+			return (s1[index] - s2[index]);
 		else
 			return (0);
 	}
+	index++;
 	return (0);
 }
