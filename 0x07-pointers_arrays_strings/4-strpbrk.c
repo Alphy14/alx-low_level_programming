3f15062cc278544984f6c8c2d1d1e46ac1b0187a
Alfred <alfredoseibossman99@gmail.com>
2023-04-03 16:10:23 +0000

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strpbrk - locates the first occurrence in the string s
- * of any of the bytes in the string accept
- * @s: String
- * @accept: String
- * Return: pointer to the byte in s that matches one of the bytes in accept,
- * or NULL if no such byte is found
- */
+* _strpbrk - locates the first occurrence in the string s
+* of any of the bytes in the string accept
+* @s: String
+* @accept: String
+* Return: pointer to the byte in s that matches one of the bytes in accept,
+* or NULL if no such byte is found
+*/
 char *_strpbrk(char *s, char *accept)
 {
 	int index;
