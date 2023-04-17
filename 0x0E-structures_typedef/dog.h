151275d647ed4943057efbc5f92b212e0ae0dcfe
Alfred <alfredoseibossman99@gmail.com>
2023-04-17 16:09:12 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,9 +20,11 @@ void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner)
+
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
