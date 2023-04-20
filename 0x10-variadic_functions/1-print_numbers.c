65aa346ffcfb691bbf55bdb3ea7a970431e05d39
Alfred <alfredoseibossman99@gmail.com>
2023-04-20 07:14:49 -0700

    updated1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list b;
 
 	if (separator == NULL)
-		return;
+		separator = "";
 	va_start(b, n);
 
 	for (i = 0; i < n; i++)
